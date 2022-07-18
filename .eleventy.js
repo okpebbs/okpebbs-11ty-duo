d4cbafc0d3278409a0330c0514316390c49a5d9b
okpebbs <32509978+okpebbs@users.noreply.github.com>
2022-07-18 10:04:05 -0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -60,7 +60,7 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addFilter('readableDate', (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
-      'dd LLL yyyy'
+      'yyyy-LL-dd'
     );
   });
 
